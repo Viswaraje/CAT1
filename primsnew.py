7a54ffe1842005adc2731389d2fe073de9fdde79
Viswaraje V <104247898+Viswaraje@users.noreply.github.com>
2023-01-18 11:10:47 +0530

    Add files via upload

---
+++ primsnew.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+import heapq
+
+
+def prims(graph, starting_vertex):
+    mst = defaultdict(set)
+    visited = set([starting_vertex])
+    edges = [
+        (cost, starting_vertex, to)
+        for to, cost in graph[starting_vertex].items()
+    ]
+    heapq.heapify(edges)
+    min_cost = 0 
+    
+    while edges:
+        cost, frm, to = heapq.heappop(edges)
+        min_cost += cost 
+        if to not in visited:
+            visited.add(to)
+            mst[frm].add(to)
+            for to_next, cost in graph[to].items():
+                if to_next not in visited:
+                    heapq.heappush(edges, (cost, to, to_next))
+    
+    print("min cost : {}".format(min_cost))
+    return mst
+
+g = {
+    'A': {'C': 2},
+    'B': {'A': 1},
+    'C': {'B': -2},
+    'D': {'A': -4, 'C': -1},
+    'E': {'D': 1},
+    'S': {'A': 10, 'E': 8}
+}
+
+print(dict(prims(g, 'S')))
